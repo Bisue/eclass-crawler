456ad33be9b4a55a0477d8894432d86e1593a8ae
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-09-11 01:59:24 +0900

    using headless browser

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { save } from '@src/utilities/save';
 
 (async () => {
   // puppeteer 윈도우 생성
-  const browser = await puppeteer.launch({ headless: false, defaultViewport: { width: 1920, height: 1080 } });
+  const browser = await puppeteer.launch({ headless: true, defaultViewport: { width: 1920, height: 1080 } });
   // crawler 인스턴스 생성
   const crawler = new EclassCrawler(browser);
 
